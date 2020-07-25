5c4c204a581f5a27c9fb5bcafa1b2c254c5f1b5a
Song Chiwon <iamchiwon@gmail.com>
2020-07-25 15:30:51 +0900

    create MemoryRepository

---
+++ MyToDo/MyToDo/ToDoService/MemoryRepository.swift
@@ -0,0 +1,49 @@
+//
+//  MemoryRepository.swift
+//  MyToDo
+//
+//  Created by Chiwon Song on 2020/07/25.
+//  Copyright © 2020 Chiwon Song. All rights reserved.
+//
+
+import Foundation
+
+class MemoryRepository : Repository {
+    var todos: [Int: ToDo] = [:]
+    
+    func create(item: ToDo, completed: @escaping (Bool) -> Void) {
+        guard todos[item.id] == nil else {
+            completed(false)
+            return
+        }
+        
+        todos[item.id] = item
+        completed(true)
+    }
+    
+    func read(completed: @escaping ([ToDo]) -> Void) {
+        completed(todos.compactMap { $1 })
+    }
+    
+    func update(item: ToDo, completed: @escaping (Bool) -> Void) {
+        guard todos[item.id] != nil else {
+            completed(false)
+            return
+        }
+        
+        todos[item.id] = item
+        completed(true)
+    }
+    
+    func delete(item: ToDo, completed: @escaping (Bool) -> Void) {
+        guard todos[item.id] != nil else {
+            completed(false)
+            return
+        }
+        
+        todos.removeValue(forKey: item.id)
+        completed(true)
+    }
+    
+    
+}
