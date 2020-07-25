dcc2349fd929d534e64c2414df2caa9e87b55ed5
Song Chiwon <iamchiwon@gmail.com>
2020-07-25 17:37:27 +0900

    create TitleInputViewController

---
+++ MyToDo/MyToDo/MyToDo/TitleInputViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TitleInputViewController.swift
+//  MyToDo
+//
+//  Created by Chiwon Song on 2020/07/25.
+//  Copyright © 2020 Chiwon Song. All rights reserved.
+//
+
+import UIKit
+
+class TitleInputViewController: UIViewController {
+    var onTitleInput: ((String) -> Void)?
+
+    @IBOutlet var titleTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self,
+                                                         action: #selector(onBackgroundTouched)))
+    }
+
+    @objc func onBackgroundTouched() {
+        view.endEditing(true)
+    }
+
+    @IBAction func onCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func onDone() {
+        guard let text = titleTextField.text,
+            let callback = onTitleInput else {
+            onCancel()
+            return
+        }
+
+        callback(text)
+        dismiss(animated: true, completion: nil)
+    }
+}
