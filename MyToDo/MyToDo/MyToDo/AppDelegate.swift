d690a547c89c2008039e82ee1604db0264138de9
Song Chiwon <iamchiwon@gmail.com>
2020-07-25 17:10:21 +0900

    inject service to ViewController manually

--- MyToDo/MyToDo/MyToDo/AppDelegate.swift
+++ MyToDo/MyToDo/MyToDo/AppDelegate.swift
@@ -10,28 +10,26 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 13, *) {
+            // SceneDeledate
+
+        } else {
+            let reposity: Repository = MemoryRepository()
+            let service: ToDoService = ToDoServiceImpl(repository: reposity)
+
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let navi = storyboard.instantiateInitialViewController() as? UINavigationController
+            let viewController = navi?.viewControllers.first as? ViewController
+            viewController?.service = service
+
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = navi
+            window?.makeKeyAndVisible()
+        }
+
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
-
