3acb1f47f04dfe54368dc840e7e0357c2f05450b
Song Chiwon <iamchiwon@gmail.com>
2020-07-25 13:49:29 +0900

    setup MyToDo xode project

---
+++ MyToDo/MyToDo/MyToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyToDo
+//
+//  Created by Chiwon Song on 2020/07/25.
+//  Copyright © 2020 Chiwon Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
